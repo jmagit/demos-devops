ab627c3ca029ea56f6b2397fb74173eedecc9fb9
Javier <35864946+jmagit@users.noreply.github.com>
2025-04-29 09:33:39 +0200

    add approval tests

--- src/test/java/com/gildedrose/FuncionalesTest.java
+++ src/test/java/com/gildedrose/FuncionalesTest.java
@@ -1,7 +1,12 @@
 package com.gildedrose;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+import java.util.Arrays;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 class FuncionalesTest {
@@ -40,6 +45,34 @@ class FuncionalesTest {
         GildedRose app = new GildedRose(items_in);
         app.updateQuality();
         assertArrayEquals(items_out, app.getItems());
+	}
+        
+	@Test
+        @Tag("smoke")
+	void instantanea() throws ProductListException {
+		Item[] items = new Item[] { 
+				new Item("+5 Dexterity Vest", 10, 20), 
+				new Item("Aged Brie", 2, 0),
+				new Item("Elixir of the Mongoose", 5, 7), 
+				new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+				new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+				new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+				// this conjured item does not work properly yet
+				new Item("Conjured Mana Cake", 3, 6) 
+				};
+
+		GildedRose app = new GildedRose(items);
+		StringBuilder output = new StringBuilder();
+		output.append("day,name, sellIn, quality\n");
+                Arrays.stream(items).forEach(item -> output.append("0," + item + "\n"));
+		for (int i = 1; i <= 31; i++) {
+			app.updateQuality();
+			for(Item item: items)
+				output.append(i + "," + item + "\n");
+		}
+		Approvals.verify(output);
 	}
 
 }
